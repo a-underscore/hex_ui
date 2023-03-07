9b9ab10409c39307894e3926fa0ed07585819d8e
Brendan Bortz <brendansbortz@protonmail.com>
2023-03-07 13:02:07 -0500

    b

--- src/ui/button.rs
+++ src/ui/button.rs
@@ -1,23 +1,26 @@
 use super::{Callback, Ui, Update};
-use crate::UiManager;
+use crate::{ScreenPos, UiManager};
 use hex::{
     anyhow,
     cgmath::Vector2,
-    components::{Camera, Transform},
     glium::glutin::event::{ElementState, Event, MouseButton, WindowEvent},
     hecs::{ev::Control, Ev},
 };
 
+#[derive(Clone)]
 pub struct Button {
     pub max: Vector2<f32>,
     pub min: Vector2<f32>,
 }
 
-impl Button {}
-
 impl Ui for Button {
-    fn ui(&mut self, _manager: &mut UiManager) -> anyhow::Result<Update> {
-        Ok(Box::new(|e, event, world| {
+    fn ui(&mut self, manager: &mut UiManager) -> anyhow::Result<Update> {
+        let max = self.max;
+        let min = self.min;
+        let window_dims = manager.window_dims;
+        let mouse_pos = manager.mouse_pos;
+
+        Ok(Box::new(move |e, event, world| {
             if let Ev::Event(Control {
                 event:
                     Event::WindowEvent {
@@ -32,31 +35,26 @@ impl Ui for Button {
                 flow: _,
             }) = event
             {
-                let p = world
-                    .em
-                    .entities
-                    .keys()
-                    .cloned()
-                    .find_map(|e| {
-                        Some((
-                            world
-                                .cm
-                                .get::<Camera>(e, &world.em)
-                                .and_then(|c| c.active.then_some(c.clone()))?,
-                            world
-                                .cm
-                                .get::<Transform>(e, &world.em)
-                                .and_then(|t| t.active.then_some(t.clone()))?,
-                        ))
-                    })
-                    .and_then(|_| /* TODO */ todo!());
+                let p = world.cm.get::<ScreenPos>(e, &world.em).and_then(|s| {
+                    let min = s.pos + min;
+                    let max = s.pos + max;
+                    let mouse_pos = Vector2::new(
+                        mouse_pos.0 / window_dims.0 as f32,
+                        mouse_pos.1 / window_dims.1 as f32,
+                    );
 
-                if let Some(Callback(ref mut mp)) =
-                    world.cm.get_mut::<Callback<Vector2<f32>>>(e, &world.em)
-                {
-                    *mp = p;
+                    (mouse_pos.x > min.x
+                        && mouse_pos.x < max.x
+                        && mouse_pos.y > min.y
+                        && mouse_pos.y < max.y)
+                        .then_some(mouse_pos)
+                });
+
+                if let Some(c) = world.cm.get_mut::<Callback<Vector2<f32>>>(e, &world.em) {
+                    c.value = p;
                 }
             }
+
             Ok(())
         }))
     }
--- src/ui/callback.rs
+++ src/ui/callback.rs
@@ -1,6 +1,9 @@
 use hex::{cid, hecs::component_manager::Component};
 
-pub struct Callback<T>(pub Option<T>);
+#[derive(Default)]
+pub struct Callback<T> {
+    pub value: Option<T>,
+}
 
 impl<T> Component for Callback<T> {
     fn id() -> usize {
